355901e93a4eee9cee3522fc42ceb8f32f7b3fce
Tom <thomas.spencer@codurance.com>
2021-02-18 12:24:23 +0000

    Add index.test

--- modules/e26-reciprocal-cycles/test/index.test.ts
+++ modules/e26-reciprocal-cycles/test/index.test.ts
@@ -7,10 +7,10 @@ import { cycleCount } from '../src';
 // 6. Stop when new numerator === is equal to original numerator * 10 / denominator
 describe('cycleCount', () => {
   it.each([
-    // [2, 0],
-    // [3, 1],
-    // [4, 0],
-    // [6, 1],
+    [2, 0],
+    [3, 1],
+    [4, 0],
+    [6, 1],
     [7, 6],
   ])('returns 0 for 2', (denominator: number, expected: number) => {
     expect(cycleCount(denominator)).toEqual(expected);
