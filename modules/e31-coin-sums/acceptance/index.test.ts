64e8e223c8691dc34019730462b022a62900a121
Tom <thomas.spencer@codurance.com>
2021-02-23 21:46:16 +0000

    Add waysOfChange iterative

--- modules/e31-coin-sums/acceptance/index.test.ts
+++ modules/e31-coin-sums/acceptance/index.test.ts
@@ -2,12 +2,12 @@ import { waysOfChangeFor } from '../src';
 
 describe('waysOfChangeFor', () => {
   it('returns the number of ways of making change for £2', () => {
-    const result = waysOfChangeFor(10, 0, []);
+    const result = waysOfChangeFor(10);
     expect(result).toEqual(11);
   });
 
-  // it('returns the number of ways of making change for £2', () => {
-  //   const result = waysOfChangeFor(200, 0, []);
-  //   expect(result).toEqual(73682);
-  // });
+  it('returns the number of ways of making change for £2', () => {
+    const result = waysOfChangeFor(200);
+    expect(result).toEqual(73682);
+  });
 });
--- modules/e31-coin-sums/src/index.ts
+++ modules/e31-coin-sums/src/index.ts
@@ -1,20 +1,18 @@
 const coins: number[] = [1, 2, 5, 10, 20, 50, 100, 200];
 const sums: Set<string> = new Set();
 
-export function waysOfChangeFor(amount: number, coin: number, route: number[]): number {
-  if (amount === 0) {
-    const numbers: number[] = route.sort((a, b) => a - b);
-    sums.add(JSON.stringify(numbers));
-
-    return 0;
+export function waysOfChangeFor(target: number): number {
+  const ways: number[] = [1];
+  for (let i = 1; i < target + 1; i += 1) {
+    ways[i] = 0;
   }
 
-  if (amount < 0) {
-    return 0;
+  for (let i = 0; i < coins.length; i += 1) {
+    const coin = coins[i];
+    for (let j = coin; j <= target; j += 1) {
+      ways[j] += ways[j - coin];
+    }
   }
 
-  coins.forEach((c) => {
-    waysOfChangeFor(amount - c, c, [c, ...route]);
-  });
-  return sums.size;
+  return ways[target];
 }
