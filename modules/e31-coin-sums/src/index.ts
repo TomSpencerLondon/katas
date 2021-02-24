5507041787ebc56a839b559cab8259f6a9dfe3e5
Tom <thomas.spencer@codurance.com>
2021-02-24 14:31:06 +0000

    Add ways of change

--- modules/e31-coin-sums/src/index.ts
+++ modules/e31-coin-sums/src/index.ts
@@ -2,6 +2,28 @@
 const coins: number[] = [1, 2, 5, 10, 20, 50, 100, 200];
 const sums: Set<string> = new Set();
 
+
+// const coins: number[] = [1, 2, 5, 10, 20, 50, 100, 200];
+// const sums: Set<string> = new Set();
+//
+// export function waysOfChangeFor(amount: number, coin: number, route: number[]): number {
+//   if (amount === 0) {
+//     const numbers: number[] = route.sort((a, b) => a - b);
+//     sums.add(JSON.stringify(numbers));
+//
+//     return 0;
+//   }
+//
+//   if (amount < 0) {
+//     return 0;
+//   }
+//
+//   coins.forEach((c) => {
+//     waysOfChangeFor(amount - c, c, [c, ...route]);
+//   });
+//   return sums.size;
+// }
+
 export function waysOfChangeFor(target: number): number {
   // eslint-disable-next-line no-shadow
   const ways: number[] = [1];
