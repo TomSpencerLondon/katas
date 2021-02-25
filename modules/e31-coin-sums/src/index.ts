329a0aea690162ebb1cf82ab7234eddbf1573c67
Tom <thomas.spencer@codurance.com>
2021-02-25 10:48:10 +0000

    add workings for coin change

--- modules/e31-coin-sums/src/index.ts
+++ modules/e31-coin-sums/src/index.ts
@@ -1,7 +1,5 @@
-
 const coins: number[] = [1, 2, 5, 10, 20, 50, 100, 200];
 const sums: Set<string> = new Set();
-
 
 // const coins: number[] = [1, 2, 5, 10, 20, 50, 100, 200];
 // const sums: Set<string> = new Set();
@@ -30,7 +28,11 @@ export function waysOfChangeFor(target: number): number {
   for (let i = 1; i < target + 1; i += 1) {
     ways[i] = 0;
   }
-
+  //  0  1  2  3  4  5
+  // [1, 0, 0, 0, 0, 0]; start
+  // [1, 1, 1, 1, 1, 1]; 1p
+  // [1, 1, 2, 2, 3, 3]; 2p
+  // [1, 1, 2, 2, 3, 4]; 5p;
   for (let i = 0; i < coins.length; i += 1) {
     const coin = coins[i];
     for (let j = coin; j <= target; j += 1) {
@@ -51,12 +53,23 @@ const getNumberOfCombinationsForCoins = (amount: number, remainingCoins: number[
   const otherCoins = [...remainingCoins];
   const largestCoin = otherCoins.pop() as number;
 
-  const degreesOfFreedomForLargestCoin = Math.floor(amount / largestCoin);
+  const degreesOfFreedomForLargestCoin = Math.floor(amount / largestCoin) + 1;
 
   return Array
-    .from({ length: degreesOfFreedomForLargestCoin + 1 })
+    .from({ length: degreesOfFreedomForLargestCoin })
     .map((_, i) => getNumberOfCombinationsForCoins(amount - (i * largestCoin), otherCoins))
     .reduce((a, b) => a + b);
 };
 
 export const getNumberOfCombinations = (amount: number): number => getNumberOfCombinationsForCoins(amount, COINS);
+
+// 5, [1, 2, 5, 10, 20, 50, 100, 200]
+
+// Amount     Largest Coin    Degrees of Freedom    Next call                                                           Return
+//   5            200             0                    5, [1, 2, 5, 10, 50, 100]
+//   5            100             0                    5, [1, 2, 5, 10, 50]
+//   5            50              0                    5, [1, 2, 5, 10]
+//   5            10              0                    5, [1, 2, 5]
+//   5            5               2                    First call: 5, [1, 2] Second call: 0, [1, 2]
+// 1st // 5       2               3                    First call: 5, [1] Second call: 3, [1], Third call: 0, [1]       1 + 1 + 1
+// 2nd // 0       2               1                    0, [1]                                                           1
