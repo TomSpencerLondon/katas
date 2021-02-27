d411da42997894099dda02de00e0729f20ddc8f0
Tom <thomas.spencer@codurance.com>
2021-02-27 14:10:45 +0000

    Add pandigitalProductSum with getAllPermutations

--- modules/e32-pandigital-products/acceptance/index.test.ts
+++ modules/e32-pandigital-products/acceptance/index.test.ts
@@ -1,8 +1,17 @@
-import { pandigitalProductSum } from '../src';
+import { pandigitalProductSum, pandigitalProductSum2 } from '../src';
 
 describe('pandigitalProductSum', () => {
   it('returns the product of the sum of the numbers which are pandigital', () => {
     const result = pandigitalProductSum();
+    expect(result).toEqual(45228);
+  });
+});
+
+
+
+describe('pandigitalProductSum2', () => {
+  it('returns the product of the sum of the numbers which are pandigital', () => {
+    const result = pandigitalProductSum2();
     expect(result).toEqual(45228);
   });
 });
--- modules/e32-pandigital-products/src/index.ts
+++ modules/e32-pandigital-products/src/index.ts
@@ -15,6 +15,25 @@ export function pandigitalProductSum(): number {
   return Array.from(products).reduce((acc, number) => acc + number, 0);
 }
 
+export function getAllPermutations(input: string): string[] {
+  if (input.length === 1) {
+    return [input];
+  }
+
+  const result: string[] = [];
+
+  for (let i = 0; i < input.length; i += 1) {
+    const firstChar = input[i];
+    const charsLeft = input.substring(0, i) + input.substring(i + 1);
+    const innerPermutatations = getAllPermutations(charsLeft);
+    innerPermutatations.forEach((str) => {
+      result.push(firstChar + str);
+    });
+  }
+
+  return result;
+}
+
 // 1, 2, 3, 4, 5, 6, 7, 8, 9
 // 1. check if string is pandigital
 // 2. Nested loop each step check if pandigital
@@ -23,4 +42,26 @@ export function pandigitalCheck(input: string): boolean {
   const digits: Set<string> = new Set(input.split('')
     .filter((el) => parseInt(el, 10) > 0));
   return digits.size === input.length;
+}
+
+export function pandigitalProductSum2(): number {
+  const products: Set<number> = new Set();
+
+  const allPermutations: string[] = getAllPermutations('123456789');
+
+  allPermutations.forEach((permutation) => {
+    for (let i = 1; i < 5; i += 1) {
+      for (let j = i; j < i + 5; j += 1) {
+        const multiplier = parseInt(permutation.substring(0, i), 10);
+        const multiplicand = parseInt(permutation.substring(i, j), 10);
+        const result = parseInt(permutation.substring(j), 10);
+
+        if (multiplier * multiplicand === result) {
+          products.add(result);
+        }
+      }
+    }
+  });
+
+  return Array.from(products).reduce((acc, number) => acc + number, 0);
 }
