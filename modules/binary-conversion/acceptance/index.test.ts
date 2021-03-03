21e6bef76017ecd38fb0904e1264d04b8cc873e3
Tom <thomas.spencer@codurance.com>
2021-03-03 11:19:09 +0000

    add code for binary-conversion

--- modules/binary-conversion/acceptance/index.test.ts
+++ modules/binary-conversion/acceptance/index.test.ts
@@ -1,4 +1,10 @@
-import { bitsNeeded, convertToBinary } from '../src';
+import {
+  bitsNeeded,
+  convertNumberToBinary,
+  convertToBinary,
+  decimalConversionToBinary,
+  fullFloatToBinary
+} from '../src';
 
 describe('convertToBinary', () => {
   it('converts 8 decimal to binary', () => {
@@ -17,6 +23,12 @@ describe('convertToBinary', () => {
 
     expect(number).toEqual(1111111011011);
   });
+
+  it('converts decimal to binary', () => {
+    const number = convertToBinary(5894);
+
+    expect(number).toEqual(1011100000110);
+  });
 });
 
 describe('bitsNeeded', () => {
@@ -24,5 +36,27 @@ describe('bitsNeeded', () => {
     const number = bitsNeeded(2.8, 14);
 
     expect(number).toEqual(48);
+  });
+});
+
+describe('decimalConversionToBinary', () => {
+  it('converts decimal fraction to binary', () => {
+    const number = decimalConversionToBinary(0.376, 10);
+
+    expect(number).toEqual(0.0110000001);
+  });
+
+  it('converts decimal fraction to binary', () => {
+    const number = decimalConversionToBinary(0.0387, 23);
+
+    expect(number).toEqual(0.00001001111010000011);
+  });
+});
+
+describe('fullFloatToBinary', () => {
+  it('converts decimal fraction to binary', () => {
+    const number = fullFloatToBinary(5894.376);
+
+    expect(number).toEqual('1011100000110.0110000001');
   });
 });
--- modules/binary-conversion/src/index.ts
+++ modules/binary-conversion/src/index.ts
@@ -22,3 +22,38 @@ export function convertToBinary(input: number): number {
 export function bitsNeeded(value: number, exponent: number): number {
   return Math.ceil((Math.log(value) + exponent * Math.log(10)) / Math.log(2));
 }
+
+export function convertNumberToBinary(input: number): string {
+  // eslint-disable-next-line no-bitwise
+  return (input >>> 0).toString(2);
+}
+
+export function decimalConversionToBinary(input: number, precision: number): number {
+  let amount: number = input;
+  const digits: number = precision;
+  let i: number = 0;
+  const result: number[] = [];
+  while (amount !== 0 && i !== digits) {
+    i += 1;
+    const m: number = 2 * amount;
+    if (m > 1) {
+      result.push(1);
+    } else {
+      result.push(0);
+    }
+    const intPart = Math.trunc(m);
+    const floatPart = Number((m - intPart));
+    amount = floatPart;
+  }
+
+  return parseFloat(`0.${result.join('')}`);
+}
+
+export function fullFloatToBinary(input: number): string {
+  const intPart = Math.trunc(input);
+  const floatPart = Number((input - intPart));
+  const integer = convertToBinary(intPart);
+  const decimal = decimalConversionToBinary(floatPart, 10);
+
+  return `${integer}${decimal.toString().substring(1)}`;
+}
